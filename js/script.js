109820740d400ee987fa4299cff9d61b6034dd68
nicollaspereira155 <nicollaspereira155@gmail.com>
2026-09-21 20:01:03 +0000

    Landing Page do projeto finalizada, bbotão voltar ao topo adicionado, animação no scroll adicionada.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Botão para voltar ao topo
+const btnVoltarTopo = document.querySelector('.btn-voltar-topo');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        btnVoltarTopo.style.display = 'block';
+    } else {
+        btnVoltarTopo.style.display = 'none';
+    }
+});
+
+btnVoltarTopo.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
